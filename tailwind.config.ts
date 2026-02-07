24868aa60eb316d77497f25beb27f6841f79254f
gogbajbobo <grigoriev.maxim@gmail.com>
2026-02-07 19:56:33 +0300

    add config files

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+// tailwind.config.ts
+
+import type { Config } from 'tailwindcss'
+
+export default <Config>{
+  content: [], // Nuxt UI расширяет автоматически
+  theme: {
+    extend: {},
+  },
+}
